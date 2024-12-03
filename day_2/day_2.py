b2e509d8fe5ff8d3e7381e67ce733e293aa5acab
Groovylein <kiriakos.antoniadis.kiri@gmail.com>
2024-12-03 16:22:33 +0100

    Day 2 first part solved

--- day_2/day_2.py
+++ day_2/day_2.py
@@ -3,11 +3,50 @@ def split_input(inp):
     l_of_l = []
     tmp_list = inp.strip().split("\n")
     for elem in tmp_list:
-        l_of_l.append(elem.split())
-    print(l_of_l)
+        tmp_list_2 = []
+        for e in elem.split():
+            tmp_list_2.append(int(e))
+        l_of_l.append(tmp_list_2)
     return l_of_l
 
+def get_safe_reports(l_of_l):
+    safe_list = []
+    for l in l_of_l:
+        safe = False
+        for i, elem in enumerate(l):
+            if (i+1) <= (len(l)-1):
+                diff = elem - l[i+1]
+                if diff in range(-4, 0):
+                    safe = check_order(l, decreasing=False)
+                    break
+                elif diff in range(1, 4):
+                    safe = check_order(l, decreasing=True)
+                    break
+                else:
+                    break
+        if safe:
+            safe_list.append(l)
+    return safe_list
+
+def check_order(input_list, decreasing=True):
+    comp = True
+    comp_list = sorted(input_list, reverse=decreasing)
+    for i, elem in enumerate(input_list):
+        if elem != comp_list[i]:
+            comp = False
+        else:
+            if (i+1) <= (len(input_list)-1):
+                diff = abs(elem - input_list[i+1])
+                if diff > 3 or diff == 0:
+                    comp = False
+    return comp
 
 with open('input.txt', 'r') as file:
     inp = file.read()
 # print(inp)
+
+list_of_lists = split_input(inp)
+# print(list_of_lists)
+safe_reports = get_safe_reports(list_of_lists)
+# print(safe_reports)
+print(f"Part 1: {len(safe_reports)}")
--- day_2/test_day_2.py
+++ day_2/test_day_2.py
@@ -1,3 +1,5 @@
+import pytest
+
 import day_2
 
 
@@ -13,3 +15,28 @@ def test_input_split():
         for sub_elem in elem:
             assert isinstance(sub_elem, int)
 
+def test_get_safe_reports():
+    list_of_lists = day_2.split_input(inp)
+    res_list_of_lists = day_2.get_safe_reports(list_of_lists)
+
+    print(res_list_of_lists)
+    assert isinstance(res_list_of_lists, list)
+    for elem in res_list_of_lists:
+        assert isinstance(elem, list)
+    assert len(res_list_of_lists) == 2
+    assert res_list_of_lists[0] == [7, 6, 4, 2, 1]
+    assert res_list_of_lists[1] == [1, 3, 6, 7, 9]
+
+testdata_increasing = [
+    ([7, 6, 4, 2, 1], False, False),
+    ([1, 3, 6, 7, 9], True, False),
+    ([1, 2, 3, 4, 5], True, False),
+    ([7, 6, 4, 2, 1], True, True),
+    ([1, 3, 6, 7, 9], False, True),
+    ([1, 0, -2, -3, -5], True, True),
+]
+@pytest.mark.parametrize("input_list, result, decreasing", testdata_increasing)
+def test_check_increasing(input_list, result, decreasing):
+    ret_value = day_2.check_order(input_list, decreasing)
+
+    assert ret_value == result
