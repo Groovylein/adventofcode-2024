a540c95bff0b3ba53bb76677e0c9edc5580ca152
Groovylein <kiriakos.antoniadis.kiri@gmail.com>
2024-12-03 08:59:57 +0100

    BROKEN: input transform test

--- day_2/day_2.py
+++ day_2/day_2.py
@@ -1,3 +1,13 @@
+
+def split_input(inp):
+    l_of_l = []
+    tmp_list = inp.strip().split("\n")
+    for elem in tmp_list:
+        l_of_l.append(elem.split())
+    print(l_of_l)
+    return l_of_l
+
+
 with open('input.txt', 'r') as file:
     inp = file.read()
-
+# print(inp)
--- day_2/test_day_2.py
+++ day_2/test_day_2.py
@@ -3,3 +3,13 @@ import day_2
 
 with open('test_input.txt', 'r') as file:
     inp = file.read()
+
+def test_input_split():
+    list_of_lists = day_2.split_input(inp)
+
+    assert isinstance(list_of_lists, list)
+    for elem in list_of_lists:
+        assert isinstance(elem, list)
+        for sub_elem in elem:
+            assert isinstance(sub_elem, int)
+
