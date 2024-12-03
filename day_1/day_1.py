534c0dc4212ab5a738c0057c3564d08ec3a08f6b
Groovylein <kiriakos.antoniadis.kiri@gmail.com>
2024-12-03 07:40:59 +0100

    day_1 part 1 complete

--- day_1/day_1.py
+++ day_1/day_1.py
@@ -1,4 +1,3 @@
-
 def transform(inp):
     l_1 = []
     l_2 = []
@@ -8,7 +7,19 @@ def transform(inp):
         l_2.append(int(l_inp[i + 1]))
     return l_1, l_2
 
+def distance_of_elem(list_1, list_2):
+    ret_list = []
+    sorted_list_1 = sorted(list_1)
+    sorted_list_2 = sorted(list_2)
+    for i in range(0, len(list_1)):
+        ret_list.append(abs(sorted_list_1[i]-sorted_list_2[i]))
+    return ret_list
 
-with open('./input.txt', 'r') as file:
+with open('input.txt', 'r') as file:
     inp = file.read()
-print(inp)
+list_1, list_2 = transform(inp)
+dist_list = distance_of_elem(list_1, list_2)
+sum = 0
+for elem in dist_list:
+    sum += elem
+print(sum)
--- day_1/test_day_1.py
+++ day_1/test_day_1.py
@@ -1,7 +1,7 @@
 import day_1
 
 
-with open('./input.txt', 'r') as file:
+with open('test_input.txt', 'r') as file:
     inp = file.read()
 
 def test_input_two_lists():
@@ -16,3 +16,11 @@ def test_input_two_lists():
         assert isinstance(elem, int)
     for elem in list_2:
         assert isinstance(elem, int)
+
+def test_distance_of_elements():
+    list_1, list_2 = day_1.transform(inp)
+    res_list = day_1.distance_of_elem(list_1, list_2)
+
+    assert isinstance(res_list, list)
+    assert len(res_list) == 6
+    assert res_list == [2 , 1 , 0 , 1 , 2 , 5]
