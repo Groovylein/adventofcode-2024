1addd66d637e99f8b3cc0c1ea60dacc39b27563a
Groovylein <kiriakos.antoniadis.kiri@gmail.com>
2024-12-03 07:21:47 +0100

    Added requirements.txt

--- day_1/test_day_1.py
+++ day_1/test_day_1.py
@@ -9,6 +9,6 @@ def test_input_two_lists():
     list_1, list_2 = day_1.transform(inp)
     
     assert isinstance(list_1, list)
-    assert isinstance(list_2, list)w
+    assert isinstance(list_2, list)
     assert len(list_1) == 6
     assert len(list_2) == 6
