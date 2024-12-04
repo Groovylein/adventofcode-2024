778e0c26a89d3f6385cd00580fe7163d2d0f8901
Groovylein <kiriakos.antoniadis.kiri@gmail.com>
2024-12-04 12:43:09 +0100

    Day 3 part 2 broken

--- day_3/day_3.py
+++ day_3/day_3.py
@@ -6,9 +6,25 @@ def mul(x: int, y: int) -> int:
 def extract_instructions(instr):
     return re.findall(r"mul\(\d+,\d+\)", instr)
 
+def extract_instructions_enhanced(instr):
+    r = re.findall(r"do\(\).(mul\(\d+,\d+\))", instr)
+    return r
+
+def get_first_occurence_of_dont(instr):
+
+    pass
+
 def caculate_correctly(instr):
     sum = 0
     l_of_operations = extract_instructions(instr)
+    for elem in l_of_operations:
+        s_result = re.search('mul\((\d+),(\d+)\)', elem)
+        sum += mul(int(s_result.group(1)), int(s_result.group(2)))
+    return sum
+
+def caculate_correctly_enhanced(instr):
+    sum = 0
+    l_of_operations = [extract_instructions(instr)[0]] + extract_instructions_enhanced(instr)
     for elem in l_of_operations:
         s_result = re.search('mul\((\d+),(\d+)\)', elem)
         sum += mul(int(s_result.group(1)), int(s_result.group(2)))
@@ -19,3 +35,4 @@ with open('input.txt', 'r') as file:
 # print(inp)
 
 print(f"Part 1: {caculate_correctly(inp)}")
+print(f"Part 2: {caculate_correctly_enhanced(inp)}")
--- day_3/test_day_3.py
+++ day_3/test_day_3.py
@@ -16,3 +16,20 @@ def test_caculate_correctly():
 
     assert isinstance(i, int)
     assert i == 161
+
+def test_get_first_occurence_of_dont():
+    pass
+
+def test_extract_instructions_enhanced():
+    list_instructions = day_3.extract_instructions_enhanced(t_inp)
+
+    assert isinstance(list_instructions, list)
+    assert len(list_instructions) == 2
+    assert list_instructions[0] == "mul(2,4)"
+    assert list_instructions[0] == "mul(8,5)"
+
+def test_caculate_correctly_enhanced():
+    i = day_3.caculate_correctly_enhanced(t_inp)
+
+    assert isinstance(i, int)
+    assert i == 48
